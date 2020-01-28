4586720750a56fe8f6a1785883ffcaadb515b8df
Stas Fomin <stanislav.fomin@gmail.com>
2020-01-28 21:31:36 +0300

    sample to demo bug

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cxfreeze wtf.py --target-dir wtf.dist --include-modules=gi._error
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo pip3 install -U git+https://github.com/anthony-tuininga/cx_Freeze.git@master
